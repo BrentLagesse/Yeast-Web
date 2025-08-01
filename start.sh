3996d6b8362a32412c07693cef06f87b28b8962f
Tim <145733724+timmy0402@users.noreply.github.com>
2025-08-01 15:23:19 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,6 @@
 # Navigate to the Django project folder
 cd yeastweb
 
-# Install only necessary dependencies (remove unnecessary ones)
-apt-get update
-apt-get install -y libpq-dev  # If using PostgreSQL, for example
-
-# Ensure that static files are collected (optional, but recommended)
-python manage.py collectstatic --noinput
-
 # Start the app using Gunicorn for production
-gunicorn --workers 3 --bind 0.0.0.0:8000 yeastweb.wsgi:application
+# <module> is the name of the folder that contains wsgi.py
+gunicorn --bind=0.0.0.0 --timeout 600 yeastweb.wsgi
